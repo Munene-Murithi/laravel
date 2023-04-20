66f1d1f23f417deab80ef1a427e3bbc7f706e804
rand <nesh@nesh.nesh>
2023-04-21 00:08:53 +0300

    created migrations, model and controller

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'homepage';
+});
+
+Route::get('/register', function () {
+    return 'register';
+});
+
+Route::get('/login', function () {
+    return 'login';
 });
